30733cc73def390a34409b4832bddad6871ba940
N-M-D <cty.chuateckyong@gmail.com>
2023-03-09 17:44:16 +0800

    Added add family file upload

--- frontend/public/js/home.js
+++ frontend/public/js/home.js
@@ -2,6 +2,11 @@ const modal = document.getElementById("create-family-modal");
 const create_family_btn = document.getElementById("create-family-btn");
 const close_btn = document.getElementById("close");
 
+const s3 = new AWS.S3({
+    accessKeyId: 'AKIAUQRGPCYN6E2K4P63',
+    secretAccessKey: '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'
+  });
+
 create_family_btn.addEventListener("click", () => {
     modal.style.display = "block";
 })
